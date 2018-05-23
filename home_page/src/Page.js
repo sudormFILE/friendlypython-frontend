2ec61083011f6f68468b70aa5cf2f5fb796b3ffc
Nettikäyttäjä <sudorm@hotmail.com>
2018-05-23 16:35:38 +0300

    minor update

--- home_page/src/Page.js
+++ home_page/src/Page.js
@@ -2,11 +2,34 @@ import React, {Component} from 'react';
 
 
 class Page extends Component {
+    constructor() {
+        super();
+        this.state = {
+            "user": undefined
+        };
+        if (this.state.user === undefined) {
+            this.setState({"user": document.getElementsByName("user-auth")})
+        }
+    };
     render() {
-        return (
-<main className="container" id="Page">
-
-</main>
+    const User = () => {
+        if (this.state.user === undefined) {
+            return <div>
+                <p>You haven't logged in yet.</p>
+                <a>Login here</a>
+            </div>
+        } else {
+            return <div>
+                <p>Welcome, {this.state.user}</p>
+            </div>
+        }
+    };
+    return (
+    <main className="container" id="Page">
+        <div className="container">
+            <User/>
+        </div>
+    </main>
         );
     }
 }
